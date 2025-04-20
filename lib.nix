9af5f59bf40e60b292cf1a042c9272696ea94838
Denis Craig <admin@deniscraig.com>
2025-04-21 00:45:38 +0100

    Fix references to nix-utils functions

--- lib.nix
+++ lib.nix
@@ -2,7 +2,7 @@ inputs: let
   nixpkgs = inputs.nixpkgs.lib;
   nix-utils = inputs.nix-utils.lib;
 in rec {
-  platforms = nix-utils.forAllSystems (system: defaultFilesAttrset ./${system});
-  linuxPlatforms = nixpkgs.filterAttrs (name: value: builtins.elem name linuxSystems) platforms;
-  darwinPlatforms = nixpkgs.filterAttrs (name: value: builtins.elem name darwinSystems) platforms;
+  platforms = nix-utils.forAllSystems (system: nix-utils.defaultFilesAttrset ./${system});
+  linuxPlatforms = nixpkgs.filterAttrs (name: value: builtins.elem name nix-utils.linuxSystems) platforms;
+  darwinPlatforms = nixpkgs.filterAttrs (name: value: builtins.elem name nix-utils.darwinSystems) platforms;
 }
